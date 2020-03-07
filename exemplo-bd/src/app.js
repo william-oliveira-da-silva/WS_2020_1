fa4100d94bfa1f4a3efcfeb39423d65a30bec931
william-oliveira-da-silva <wyll.hello@gmail.com>
2020-03-07 02:48:58 -0400

    usando MongoDB

---
+++ exemplo-bd/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const bodyParser = require('body-parser');
+
+require('./db/config');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+
+/**
+ * Define que os dados recebidos no
+ * corpo da requisição devem estar
+ * no formato JSON
+ */
+ app.use(bodyParser.json());
+
+ /**
+  * Protege os acessos ao banco de dados
+  * contra injeções de consultas maliciosas
+  */
+ app.use(mongoSanitize());
+
+ /**
+  * Para teste inicial
+  */
+ app.get('/', (req,res)=> res.send('oi!'));
+ module.exports = app;
---
+++ exemplo-bd/src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const PORTA = process.env.PORT || 3000;
+
+app.listen(PORTA,
+    () => console.log(`App ouvindo na porta ${PORTA}`));
