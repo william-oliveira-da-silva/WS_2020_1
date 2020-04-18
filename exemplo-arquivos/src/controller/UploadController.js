595739e5d2cefda51d2dd3698b17949367c4ec3b
william-oliveira-da-silva <wyll.hello@gmail.com>
2020-04-18 16:52:08 -0300

    aula quarentena parte 2

--- exemplo-arquivos/src/controller/UploadController.js
+++ exemplo-arquivos/src/controller/UploadController.js
@@ -1,16 +1,44 @@
+const fs = require ('fs');
+
 const controller = {
 
     realizarUpload: (req, res) => {
         
-        const {name, size, mimetype } = req.files['arquivo'];
-        const resposta = {
-            nome: name,
-            tamanho: (size / 1024 ) / 1024,
-            tipo: mimetype
-        };
+        const {name,  mimetype, data } = req.files['arquivo'];
 
-        res.json(resposta);
+        /**
+         * Cria o nome do arquivo temporário
+         */
 
+        const nomeArquivo = `${new Date().getTime()}`;
+        console.log(nomeArquivo);
+
+        /**
+         * cria o arquivo temporário
+         */
+        fs.writeFileSync(nomeArquivo, data);
+
+        /**
+         * Cria o stream de leitura do arquivo temporário
+         */
+        const readStream = fs.createReadStream(nomeArquivo);
+
+        /**
+         * Realiza a gravação do arquivo no baco de dados
+         */
+
+        const Arquivo = require ('../models/Arquivo')
+        const metadados = {filename: name, contentType: mimetype };
+        Arquivo.write(metadados, readStream, (erro, arquivo) => {
+            fs.unlinkSync(nomeArquivo);
+            if(erro){
+                console.log(erro);
+                res.status(500).json({ erro: 'Erro ao tentar salvar o arquivo'})
+            }else{
+            res.status(201).json({mensagem: 'Arquivo salvo', id: arquivo_id});
+            }
+        })        
+        
     }
 };
 
---
+++ exemplo-arquivos/src/models/Arquivo.js
@@ -0,0 +1,7 @@
+const {createModel} = require('mongoose-gridfs');
+
+const Arquivo = createModel({
+    modelName: 'Arquivo'
+});
+
+module.exports = Arquivo;
